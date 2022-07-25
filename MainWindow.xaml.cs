0d07ea47103ff686fc8ecbe1dca9b8911e1f46a2
SickSect <sleeptboy906sb@gmail.com>
2022-07-25 12:06:39 +0300

    в процессе, создан метод загрузки окна

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace deals_app
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ data/todo_model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deals_app.data
+{
+    class todo_model
+    {
+        public DateTime Creation_date { get; set; } = DateTime.Now;
+        private bool IsDone;
+
+        public bool Is_Done
+        {
+            get { return IsDone; }
+            set { IsDone = value; }
+        }
+
+
+        private string Event_Info;
+
+        public string event_info
+        {
+            get { return Event_Info; }
+            set { Event_Info = value; }
+        }
+
+    }
+}
