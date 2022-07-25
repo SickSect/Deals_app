00f3118a948b5aa90c8e36400c9556dfea916dc6
SickSect <sleeptboy906sb@gmail.com>
2022-07-25 12:45:51 +0300

    in progress

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using deals_app.data;
 
 namespace deals_app
 {
@@ -20,6 +22,7 @@ namespace deals_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<todo_model> todo_date;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +30,13 @@ namespace deals_app
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            todo_date = new BindingList<todo_model>()
+            {
+                new todo_model(){event_info = "this is the event"},
+                new todo_model(){event_info = "second event here"}
+            };
 
+            dgTodoList.ItemsSource = todo_date;
         }
     }
 }
